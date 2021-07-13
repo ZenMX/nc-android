93f64724079fcd154d3e46e1efff72c2a7c1b167
ZhaoLinlin <linlin.zhao@mxplayer.in>
2021-07-13 18:07:50 +0800

    [ZLL] write crash info to file first, and report it on next app startup.

--- native-crash/src/main/java/com/mxtech/NCException.java
+++ native-crash/src/main/java/com/mxtech/NCException.java
@@ -1,15 +1,32 @@
 package com.mxtech;
 
+import android.text.TextUtils;
+
 import androidx.annotation.NonNull;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.LinkedList;
 
-public class NCException extends Exception {
+import okio.BufferedSink;
+import okio.BufferedSource;
+import okio.Okio;
+import okio.Sink;
+import okio.Source;
 
-    private final StackTraceElement[] traceElements;
+public class NCException extends Exception {
+    private static final String SEP = "::";
+    private static final String NULL_STR = "__0_null__";
+
+    private StackTraceElement[] traceElements;
     private String msg;
+
+    public NCException() {
+        super();
+    }
 
     public NCException(String nativeStack, StackTraceElement[] traceElements) {
         super();
@@ -40,6 +57,10 @@ public class NCException extends Exception {
     }
 
 
+    public String getMsg() {
+        return msg;
+    }
+
     @NonNull
     @Override
     public StackTraceElement[] getStackTrace() {
@@ -63,6 +84,102 @@ public class NCException extends Exception {
             }
             printStream.println();
         }
+    }
+
+    public static NCException createFromFile(File file) throws IOException {
+        String content;
+        Source source = Okio.source(file);
+        try {
+            BufferedSource buffer = Okio.buffer(source);
+            byte[] bytes = buffer.readByteArray();
+            content = new String(bytes);
+        } finally {
+            source.close();
+        }
+
+        String[] split = content.split("\n");
+        if (split.length < 4)
+            return null;
+
+        String msg = split[0];
+        LinkedList<StackTraceElement> list = new LinkedList<>();
+        for (int i = 1; i < split.length; i++) {
+            String str = split[i];
+            if (TextUtils.isEmpty(str))
+                continue;
+
+            String[] split1 = str.split(SEP);
+            if (split1.length != 4) {
+                return null;
+            }
+
+            String className = split1[0];
+            String methodName = split1[1];
+            String fileName = split1[2];
+            int lineNumber = Integer.parseInt(split1[3]);
+
+            if (TextUtils.isEmpty(className) || TextUtils.isEmpty(methodName))
+                return null;
+
+            if (TextUtils.equals(fileName, NULL_STR)) {
+                fileName = null;
+            }
+
+
+            list.add(new StackTraceElement(className, methodName, fileName, lineNumber));
+        }
+
+        NCException ncException = new NCException();
+        ncException.msg = msg;
+        ncException.traceElements = list.toArray(new StackTraceElement[0]);
+        return ncException;
+    }
+
+    public void writeToFile(File file) throws FileNotFoundException {
+        NCException exception = this;
+        Sink sink = Okio.sink(file);
+        try {
+            BufferedSink buffer = Okio.buffer(sink);
+            String msg = exception.getMsg();
+            buffer.writeUtf8(msg);
+            buffer.writeByte('\n');
+
+            StackTraceElement[] stackTrace = exception.getStackTrace();
+            for (StackTraceElement stackTraceElement : stackTrace) {
+                writeStack(buffer, stackTraceElement);
+                buffer.writeByte('\n');
+            }
+
+            buffer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                sink.close();
+            } catch (Exception ignore) {
+
+            }
+        }
+    }
+
+    private static void writeStack(BufferedSink buffer, StackTraceElement stackTraceElement) throws IOException {
+
+        String className = stackTraceElement.getClassName();
+        String methodName = stackTraceElement.getMethodName();
+        int lineNumber = stackTraceElement.getLineNumber();
+        String fileName = stackTraceElement.getFileName();
+
+        if (TextUtils.isEmpty(fileName)) {
+            fileName = NULL_STR;
+        }
+
+        buffer.writeUtf8(className);
+        buffer.writeUtf8(SEP);
+        buffer.writeUtf8(methodName);
+        buffer.writeUtf8(SEP);
+        buffer.writeUtf8(fileName);
+        buffer.writeUtf8(SEP);
+        buffer.writeUtf8(String.valueOf(lineNumber));
     }
 
 }
--- native-crash/src/main/java/com/mxtech/NativeCrashCollector.java
+++ native-crash/src/main/java/com/mxtech/NativeCrashCollector.java
@@ -4,6 +4,7 @@ import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
 
 import java.io.File;
+import java.io.IOException;
 
 import okio.BufferedSource;
 import okio.Okio;
@@ -19,47 +20,104 @@ public class NativeCrashCollector {
     }
 
     private static Callback s_callback = null;
+    private static File crashDir = null;
+
     public static void init(String dir, Callback callback) {
         s_callback = callback;
-        File file = new File(dir);
-        file.mkdirs();
+        crashDir = new File(dir);
+        crashDir.mkdirs();
         nativeInitClass(dir);
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                File[] files = file.listFiles();
-                if (files == null)
-                    return;
-
-                for (File file1 : files) {
-                    String name = file1.getName();
-                    if (!name.startsWith("nc_") || !name.endsWith(".txt")) {
-                        continue;
-                    }
-
-                    try {
-                        Source source = Okio.source(file1);
-                        BufferedSource buffer = Okio.buffer(source);
-                        byte[] bytes = buffer.readByteArray();
-                        source.close();
-                        file1.delete();
-                        String s = new String(bytes);
-                        onNativeCrash(s);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
-                }
+                listNativeCrash();
+                listNativeAndJavaCrash();
             }
         }).start();
     }
 
+    private static void listNativeCrash() {
+        File[] files = crashDir.listFiles();
+        if (files == null)
+            return;
+
+        for (File file1 : files) {
+            String name = file1.getName();
+            if (!name.startsWith("nc_") || !name.endsWith(".txt")) {
+                continue;
+            }
+
+            try {
+                Source source = Okio.source(file1);
+                BufferedSource buffer = Okio.buffer(source);
+                byte[] bytes = buffer.readByteArray();
+                source.close();
+                file1.delete();
+                String s = new String(bytes);
+                NCException ncException = createNcException(s);
+                callback(ncException);
+//                        onNativeCrash(s);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+    private static void listNativeAndJavaCrash() {
+        File dir = new File(crashDir, "nc_java");
+        File[] files = dir.listFiles();
+        if (files == null)
+            return;
+
+        for (File file1 : files) {
+            String name = file1.getName();
+            if (!name.startsWith("nc_") || !name.endsWith(".txt")) {
+                continue;
+            }
+
+            try {
+                NCException ncException = NCException.createFromFile(file1);
+                if (ncException != null) {
+                    s_callback.onNativeCrash(ncException);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            file1.delete();
+
+        }
+    }
+
     @Keep
     private static void onNativeCrash(String log) {
-        NCException ncException = new NCException(log, Thread.currentThread().getStackTrace());
-        s_callback.onNativeCrash(ncException);
+        NCException ncException = createNcException(log);
+
+        File dir = new File(crashDir, "nc_java");
+        dir.mkdirs();
+        long now = System.currentTimeMillis();
+        String fileName = "nc_" + now + ".txt";
+        File file = new File(dir, fileName);
+        try {
+            ncException.writeToFile(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        s_callback.onNativeCrash(ncException);
     }
+
+    private static void callback(NCException e) {
+        s_callback.onNativeCrash(e);
+    }
+    @NonNull
+    private static NCException createNcException(String log) {
+        return new NCException(log, Thread.currentThread().getStackTrace());
+    }
+
+
+
+
 
     private static native void nativeInitClass(String dir);
 }
